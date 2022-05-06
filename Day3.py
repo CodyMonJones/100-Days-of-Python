d3a1e5f46e6dbe18925567d2413bba936e51bbb7
Cody Jones <cody.monroe.jones@gmail.com>
2022-05-05 19:30:33 -0500

    true love added :D

--- Day3.py
+++ Day3.py
@@ -21,15 +21,37 @@
 # else:
 #     print("clinically obese")       
 # 
-year_input = int(input("Enter year you would like to check: "))
+# year_input = int(input("Enter year you would like to check: "))
 
-if year_input % 4 == 0:
-    if year_input % 100 == 0:
-        if year_input % 400 == 0:
-            print(f"The year {year_input} is a leap year!")
-        else:
-            print(f"The year {year_input} is not a leap year!")    
-    else:
-        print(f"The year {year_input} is not a leap year!")        
-else:
-    print(f"The year {year_input} is not a leap year!")
+# if year_input % 4 == 0:
+#     if year_input % 100 == 0:
+#         if year_input % 400 == 0:
+#             print(f"The year {year_input} is a leap year!")
+#         else:
+#             print(f"The year {year_input} is not a leap year!")    
+#     else:
+#         print(f"The year {year_input} is not a leap year!")        
+# else:
+#     print(f"The year {year_input} is not a leap year!")
+name1 = input("Enter the first persons name: ").lower()
+name2 = input("Enter the second persons name: ").lower()
+
+combined_names = name1 + name2
+
+t = combined_names.count("t")
+r = combined_names.count("r")
+u = combined_names.count("u")
+e = combined_names.count("e")
+
+true = t + r + u + e
+
+l = combined_names.count("l")
+o = combined_names.count("o")
+v = combined_names.count("v")
+e = combined_names.count("e")
+
+love = l + o + v + e
+
+love_score = str(true) + str(love)
+
+print(f"Your love score is: {love_score}")
