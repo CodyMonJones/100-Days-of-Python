74fdd2c7a5c4f3f9e4f4293af24c76df5de59b36
Cody Jones <cody.monroe.jones@gmail.com>
2022-05-05 19:08:24 -0500

    leap year check added

--- Day3.py
+++ Day3.py
@@ -5,18 +5,31 @@
 # else:
 #     print(f"Your number {number} is Odd")    
 
-height = float(input("Enter your height in m: "))
-weight = float(input("Enter your weight in kg: "))
+# height = float(input("Enter your height in m: "))
+# weight = float(input("Enter your weight in kg: "))
 
-bmi = round(weight / height ** 2)
+# bmi = round(weight / height ** 2)
 
-if bmi < 18.5:
-    print("Underweight")
-elif bmi < 25:
-    print("Normal weight")
-elif bmi < 30:
-    print("Overweight")
-elif bmi < 35:
-    print("Obese")
+# if bmi < 18.5:
+#     print("Underweight")
+# elif bmi < 25:
+#     print("Normal weight")
+# elif bmi < 30:
+#     print("Overweight")
+# elif bmi < 35:
+#     print("Obese")
+# else:
+#     print("clinically obese")       
+# 
+year_input = int(input("Enter year you would like to check: "))
+
+if year_input % 4 == 0:
+    if year_input % 100 == 0:
+        if year_input % 400 == 0:
+            print(f"The year {year_input} is a leap year!")
+        else:
+            print(f"The year {year_input} is not a leap year!")    
+    else:
+        print(f"The year {year_input} is not a leap year!")        
 else:
-    print("clinically obese")              
+    print(f"The year {year_input} is not a leap year!")
