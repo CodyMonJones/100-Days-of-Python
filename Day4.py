a178b49dd7d0b32c97bda9cabc54d650ca850967
Cody Jones <cody.monroe.jones@gmail.com>
2022-05-09 20:27:13 -0500

    Rock, paper, scissors

--- Day4.py
+++ Day4.py
@@ -1,25 +1,48 @@
-# import random
+import random
 
-# names_string = input("Please enter names separated by a comma: ")
-# names_list = names_string.split(",");
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images = [rock, paper, scissors]
+print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors")
+user_choice = int(input())
+if user_choice >= 3:
+    print("Number must be lower than 3")
+else:
+    cpu_choice = random.randint(0, 2)
+    print(game_images[user_choice])
+    print("Computer chose:\n")
+    print(game_images[cpu_choice])
 
-# random_name = random.randint(0, len(names_list) - 1)
-# paying_bill = names_list[random_name]
+    if user_choice == 2 and cpu_choice == 0:
+        print("Computer wins!")
+    elif user_choice == 0 and cpu_choice == 2:
+        print("You win!")
+    elif user_choice > cpu_choice:
+        print("You win!")
+    elif user_choice < cpu_choice:
+        print("Computer wins!")
+    else:
+        print("Draw!")                
 
-# print(f"{paying_bill} is paying for the bill")
-
-row1 = ["*", "*", "*"]
-row2 = ["*", "*", "*"]
-row3 = ["*", "*", "*"]
-
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-location = input("Please enter a location: ")
-
-row = int(location[0])
-col = int(location[1])
-
-selected_row = map[col - 1]
-selected_row[row - 1] = "X"
-
-print(f"{row1}\n{row2}\n{row3}")
