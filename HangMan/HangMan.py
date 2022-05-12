b334d5a03301bd69b0be3aea0be84c3e1faa9d80
Cody Jones <cody.monroe.jones@gmail.com>
2022-05-11 20:22:09 -0500

    reorganized some code

--- HangMan/HangMan.py
+++ HangMan/HangMan.py
@@ -58,6 +58,8 @@ stages = ['''
 ''']
 word_list = ["Cat", "Dog", "Mouse", "Bird", "Moose"]
 blanks_list = []
+number_of_guesses = 6
+winner = False
 
 #grabs a random word from the list and determines length
 word_to_guess = random.choice(word_list).lower()
@@ -68,14 +70,13 @@ for _ in range(word_length):
     blanks_list.append("_")
 
 #main game function
-number_of_guesses = 6
-winner = False
 while not winner:
     print(stages[number_of_guesses])
     print(blanks_list)
     print(f"Guesses left: {number_of_guesses}")
     user_letter = input("Guess a letter: ").lower()
 
+    #conditions to check if letter is in word or not
     if user_letter not in word_to_guess:
         number_of_guesses -= 1
     else:    
@@ -84,6 +85,7 @@ while not winner:
             if user_letter == letter:
                 blanks_list[position] = letter
     
+    #win or lose conditions
     if number_of_guesses == 0:
         winner = True
         print(f"{stages[number_of_guesses]}\n {number_of_guesses} guesses left the word was {word_to_guess}. Sorry you lose")
