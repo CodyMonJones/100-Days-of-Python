0c0e4ad38edb8a9dc0c13d3c4b3b6bf3e37598cc
Cody Jones <cody.monroe.jones@gmail.com>
2022-05-24 08:10:42 -0500

    Caesar Cipher

---
+++ CaeserCipher/caeser.py
@@ -0,0 +1,10 @@
+user_choice = input("Type 'e' to encrypt, type 'd' to decrypt: ")
+user_message = input("Type your message: ")
+shift_number = input("How far would you like to shift: ")
+
+
+def encryption():
+    pass
+
+def decryption():
+    pass
