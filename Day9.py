07a382cc19a026abaeb953a0b9c2c67b21069258
Cody Jones <cody.monroe.jones@gmail.com>
2022-06-16 14:14:27 -0500

    Dictionary and List nesting

---
+++ Day9.py
@@ -0,0 +1,6 @@
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lillia", "Dijon"], "total_visits" : 12},
+    "Texas" : {"cities_visited" : ["Dallas", "Austin", "Fort Worth"], "total_visits" : 3}
+}
+
+print(travel_log["France"])
