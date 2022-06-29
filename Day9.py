2c8918b604dfe82b0100d91dbbc6fde5a03b1ea2
Cody Jones <cody.monroe.jones@gmail.com>
2022-06-28 20:10:49 -0500

    Day 9

--- Day9.py
+++ Day9.py
@@ -1,6 +1,25 @@
-travel_log = {
-    "France": {"cities_visited": ["Paris", "Lillia", "Dijon"], "total_visits" : 12},
-    "Texas" : {"cities_visited" : ["Dallas", "Austin", "Fort Worth"], "total_visits" : 3}
-}
+bids = {}
 
-print(travel_log["France"])
+print("Welcome to the silent Auction!\n")
+
+bidder_name = input("What is your name? ")
+single_bid = int(input("How much would you like to bid? $"))
+
+bids[bidder_name] = single_bid
+highest_bid = 0
+new_bid = True
+
+while new_bid:
+    new_bid_check = input("Is there another bidder? 'Yes' 'No'").lower()
+    if new_bid_check == "yes":
+        bidder_name = input("What is your name? ")
+        single_bid = int(input("How much would you like to bid? $"))
+        bids[bidder_name] = single_bid
+        print(bids)
+    else:
+        new_bid = False    
+
+for bid in bids:
+    if bids[bid] > highest_bid:
+        highest_bid = bids[bid]
+print(f"The highest bid was {highest_bid} by {bid}")            
